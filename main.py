e1fc24c922e2abc90c90297ed8bff56ceb364b67
steeler-5 <beaualphonso@gmail.com>
2025-06-26 15:59:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -36,4 +36,5 @@ def update_memory():
         return jsonify({"error": str(e)}), 500
   
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
